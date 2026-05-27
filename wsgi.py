e5ec1e59032144cbab5ebac8a1d4561f9676f7e2
OSCARDAVIB <profuturoiedtng@gmail.com>
2026-05-26 22:05:49 -0500

    Create wsgi.py
    Add wsgi.py for Render deployment

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from app import create_app, db
+from flask_migrate import Migrate
+import os
+
+app = create_app()
+migrate = Migrate(app, db)
+
+# Render asigna el puerto automáticamente
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
